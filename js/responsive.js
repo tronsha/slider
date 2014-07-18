f0038787ced382343ceb4e1998706b1cc1ceecd4
Stefan Hüsges <tronsha@gmail.com>
2014-07-18 16:28:01 +0200

    add a help function for responsive

---
+++ js/responsive.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    sliderResize();
+}
+
+window.onresize = function () {
+    sliderResize();
+}
+
+var sliderResize = function() {
+    var $slider = jQuery('#slider');
+    var $sliderPrev = jQuery('#slider .prev');
+    var $sliderNext = jQuery('#slider .next');
+    var $activeImage = jQuery('#slider .slides .active');
+    var imagesWidth = $activeImage[0].naturalWidth;
+    var imagesHeight = $activeImage[0].naturalHeight;
+    var sliderWidth = $slider.width();
+    var sliderHeight = sliderWidth * imagesHeight / imagesWidth;
+    var buttonTop = sliderHeight / 2 - 7;
+    $slider.height(sliderHeight);
+    $sliderPrev.css('top', buttonTop);
+    $sliderNext.css('top', buttonTop);
+}

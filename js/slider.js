61c4d7cf5d40b5d061c75a24f8a9e1c27dab2d66
Stefan Hüsges <tronsha@gmail.com>
2014-07-18 16:24:12 +0200

    active class for z-index

--- js/slider.js
+++ js/slider.js
@@ -35,10 +35,10 @@
                     that.show(slide);
                 });
                 if (slide == that.settings.slide) {
-                    $(element).css('opacity', '1').css('z-index', '501');
+                    $(element).addClass('active').css('opacity', '1');
                     $point.addClass('active');
                 } else {
-                    $(element).css('opacity', '0').css('z-index', '500');
+                    $(element).css('opacity', '0');
                 }
             });
             $slider.hover(function () {
@@ -74,10 +74,10 @@
             }
         },
         show: function (slide) {
-            $('.image' + that.settings.slide).css('z-index', '500').animate({opacity: 0}, that.settings.delay);
+            $('.image' + that.settings.slide).removeClass('active').animate({opacity: 0}, that.settings.delay);
             $(that.element).find('.point' + that.settings.slide).removeClass('active');
             that.settings.slide = slide;
-            $('.image' + that.settings.slide).css('z-index', '501').animate({opacity: 1}, that.settings.delay);
+            $('.image' + that.settings.slide).addClass('active').animate({opacity: 1}, that.settings.delay);
             $(that.element).find('.point' + that.settings.slide).addClass('active');
             return this;
         }

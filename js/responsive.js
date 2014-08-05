767e887ae07e06efee7986d822cee506d1dfd6a7
Stefan Hüsges <Tronsha@gmx.de>
2014-08-05 16:00:58 +0200

    Update responsive.js

--- js/responsive.js
+++ js/responsive.js
@@ -12,12 +12,14 @@ var sliderResize = function() {
     var $sliderPrev = jQuery('#slider .prev');
     var $sliderNext = jQuery('#slider .next');
     var $activeImage = jQuery('#slider .slides .active');
-    var imagesWidth = $activeImage[0].naturalWidth;
-    var imagesHeight = $activeImage[0].naturalHeight;
-    var sliderWidth = $slider.width();
-    var sliderHeight = sliderWidth * imagesHeight / imagesWidth;
-    var buttonTop = sliderHeight / 2 - 7;
-    $slider.height(sliderHeight);
-    $sliderPrev.css('top', buttonTop);
-    $sliderNext.css('top', buttonTop);
+    if ($activeImage.length > 0) {
+        var imagesWidth = $activeImage[0].naturalWidth;
+        var imagesHeight = $activeImage[0].naturalHeight;
+        var sliderWidth = $slider.width();
+        var sliderHeight = sliderWidth * imagesHeight / imagesWidth;
+        var buttonTop = sliderHeight / 2 - 7;
+        $slider.height(sliderHeight);
+        $sliderPrev.css('top', buttonTop);
+        $sliderNext.css('top', buttonTop);
+    }
 }

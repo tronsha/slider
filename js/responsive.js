d34b15e772eebf758f9ae962f02dc192c2c7f904
Stefan Hüsges <tronsha@gmail.com>
2014-08-22 15:05:36 +0200

    a better solution with CSS for positioning from the next and prev button

--- js/responsive.js
+++ js/responsive.js
@@ -9,17 +9,12 @@ window.onresize = function () {
 var sliderResize = function() {
     var $slider = jQuery('#slider');
     $slider.css('max-width', '100%');
-    var $sliderPrev = jQuery('#slider .prev');
-    var $sliderNext = jQuery('#slider .next');
-    var $activeImage = jQuery('#slider .slides .active');
+    var $activeImage = jQuery('.slider .slides .active');
     if ($activeImage.length > 0) {
-        var imagesWidth = $activeImage[0].naturalWidth;
-        var imagesHeight = $activeImage[0].naturalHeight;
+        var imageWidth = $activeImage[0].naturalWidth;
+        var imageHeight = $activeImage[0].naturalHeight;
         var sliderWidth = $slider.width();
-        var sliderHeight = sliderWidth * imagesHeight / imagesWidth;
-        var buttonTop = sliderHeight / 2 - 7;
+        var sliderHeight = sliderWidth * imageHeight / imageWidth;
         $slider.height(sliderHeight);
-        $sliderPrev.css('top', buttonTop);
-        $sliderNext.css('top', buttonTop);
     }
 }

56d8c89ae15f669df222a44c3c0e1e447a3e82df
Stefan Hüsges <tronsha@gmail.com>
2014-08-31 21:06:58 +0200

    first step to multi slider

--- js/responsive.js
+++ js/responsive.js
@@ -7,14 +7,17 @@ window.onresize = function () {
 }
 
 var sliderResize = function() {
-    var $slider = jQuery('#slider');
-    $slider.css('max-width', '100%');
-    var $activeImage = jQuery('.slider .slides .active');
-    if ($activeImage.length > 0) {
-        var imageWidth = $activeImage[0].naturalWidth;
-        var imageHeight = $activeImage[0].naturalHeight;
-        var sliderWidth = $slider.width();
-        var sliderHeight = sliderWidth * imageHeight / imageWidth;
-        $slider.height(sliderHeight);
-    }
+    var $slider = jQuery('.slider');
+    $slider.each(function() {
+        var $this = jQuery(this);
+        $this.css('max-width', '100%');
+        var $activeImage = $this.find('.slides .active');
+        if ($activeImage.length > 0) {
+            var imageWidth = $activeImage[0].naturalWidth;
+            var imageHeight = $activeImage[0].naturalHeight;
+            var sliderWidth = $this.width();
+            var sliderHeight = sliderWidth * imageHeight / imageWidth;
+            $this.height(sliderHeight);
+        }
+    });
 }

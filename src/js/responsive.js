cdd69e2474b3169b3449b5c39a85960ba5e47c42
Stefan Hüsges <tronsha@gmx.de>
2017-02-24 07:49:24 +0100

    update resize

--- src/js/responsive.js
+++ src/js/responsive.js
@@ -1,55 +1,11 @@
 window.onload = function () {
-    sliderResize();
-};
-
-window.onresize = function () {
-    sliderResize();
-};
-
-var sliderResize = function () {
-    var $slider = jQuery('.slider');
-    $slider.each(function () {
-        var $this = jQuery(this);
-        var $activeImage = $this.find('.slides .active');
-        if ($activeImage.length > 0) {
-            var imageWidth = $activeImage[0].naturalWidth;
-            var imageHeight = $activeImage[0].naturalHeight;
-            var sliderWidth = $this.width();
-            var sliderHeight = sliderWidth * imageHeight / imageWidth;
-            $this.height(sliderHeight);
-            sliderResizeChange(sliderHeight);
-        }
+    jQuery('.slider').each(function () {
+        jQuery(this).data('plugin_slider').resize();
     });
 };
 
-var sliderResizeChange = function(sliderHeight) {
-    var $prevImage = jQuery('.slider .prev > img');
-    var $nextImage = jQuery('.slider .next > img');
-    if ($prevImage.length > 0) {
-        var prevImageHeight = parseInt($prevImage[0].naturalHeight);
-        $prevImage.css('max-height', prevImageHeight);
-        $prevImage.css('min-height', prevImageHeight / 2);
-        $prevImage.css('height', sliderHeight / 10);
-    }
-    if ($nextImage.length > 0) {
-        var nexImageHeight = parseInt($nextImage[0].naturalHeight);
-        $nextImage.css('max-height', nexImageHeight);
-        $nextImage.css('min-height', nexImageHeight / 2);
-        $nextImage.css('height', sliderHeight / 10);
-    }
-};
-
-var sliderResizeTextBox = function () {
-    if (window.matchMedia('(max-width: 767px)').matches) {
-        var height = parseInt(jQuery('.slider .text span.active').css('height'));
-        jQuery('.slider .text span').css('bottom', -height);
-        jQuery('.slider .text span').css('left', 0);
-        jQuery('.slider .text span').css('right', 0);
-        jQuery('.slider').css('margin-bottom', height);
-    } else {
-        jQuery('.slider .text span').css('bottom', '');
-        jQuery('.slider .text span').css('left', '');
-        jQuery('.slider .text span').css('right', '');
-        jQuery('.slider').css('margin-bottom', '');
-    }
+window.onresize = function () {
+    jQuery('.slider').each(function () {
+        jQuery(this).data('plugin_slider').resize();
+    });
 };
--- src/js/slider.js
+++ src/js/slider.js
@@ -97,25 +97,38 @@
             }
         },
         show: function (slide) {
-            $(this.element).find('.slides img:nth-child(' + this.vari.slide + ')').stop().removeClass('active').animate({opacity: 0}, this.settings.delay);
-            $(this.element).find('.position .points:nth-child(' + this.vari.slide + ')').removeClass('active');
-            $(this.element).find('.text span:nth-child(' + this.vari.slide + ')').removeClass('active');
+            var $slider = $(this.element);
+            $slider.find('.slides img:nth-child(' + this.vari.slide + ')').stop().removeClass('active').animate({opacity: 0}, this.settings.delay);
+            $slider.find('.position .points:nth-child(' + this.vari.slide + ')').removeClass('active');
+            $slider.find('.text span:nth-child(' + this.vari.slide + ')').removeClass('active');
             this.vari.slide = slide;
-            $(this.element).find('.slides img:nth-child(' + this.vari.slide + ')').stop().addClass('active').animate({opacity: 1}, this.settings.delay);
-            $(this.element).find('.position .points:nth-child(' + this.vari.slide + ')').addClass('active');
-            $(this.element).find('.text span:nth-child(' + this.vari.slide + ')').addClass('active');
+            $slider.find('.slides img:nth-child(' + this.vari.slide + ')').stop().addClass('active').animate({opacity: 1}, this.settings.delay);
+            $slider.find('.position .points:nth-child(' + this.vari.slide + ')').addClass('active');
+            $slider.find('.text span:nth-child(' + this.vari.slide + ')').addClass('active');
             if (this.settings.resize === true) {
                 this.resize();
             }
             return this;
         },
-        resize: function () {
-            var $image = $(this.element).find('.slides img:nth-child(' + this.vari.slide + ')');
-            var imageWidth = $image[0].naturalWidth;
-            var imageHeight = $image[0].naturalHeight;
-            var sliderWidth = $(this.element).width();
-            var sliderHeight = sliderWidth * imageHeight / imageWidth;
-            $(this.element).animate({height: sliderHeight}, this.settings.delay);
+        resize: function (animate) {
+            var $slider = $(this.element);
+            var $activeImage = $slider.find('.slides .active');
+            var $prevIcon = $slider.find('.prev > img');
+            var $nextIcon = $slider.find('.next > img');
+            var sliderHeight = $slider.width() * $activeImage[0].naturalHeight / $activeImage[0].naturalWidth;
+            if (animate === true) {
+                $slider.animate({height: sliderHeight}, this.settings.delay);
+            } else {
+                $slider.height(sliderHeight);
+            }
+            if ($prevIcon.length > 0) {
+                var prevIconHeight = parseInt($prevIcon[0].naturalHeight);
+                $prevIcon.css({'max-height': prevIconHeight, 'min-height': prevIconHeight / 2, 'height': sliderHeight / 10});
+            }
+            if ($nextIcon.length > 0) {
+                var nextIconHeight = parseInt($nextIcon[0].naturalHeight);
+                $nextIcon.css({'max-height': nextIconHeight, 'min-height': nextIconHeight / 2, 'height': sliderHeight / 10});
+            }
         }
     };
 
